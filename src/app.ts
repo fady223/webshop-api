a0c30d70fdcd2c795425bf5041f95cc0c5468c10
Fadi Shaker <shaker@sauter-held.de>
2023-02-15 10:40:51 +0100

    clean up

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,7 @@
-import express, { Request, Response } from "express"
+import express from "express"
 import * as dotenv from 'dotenv'
 import cors from "cors"
 import { connectToMongo } from "./mongo"
-import Order from "./models/Order"
-import Counter, { CounterEnum, getNextNumber } from "./models/Counter"
 import { orderRouter } from "./controllers/order"
 
 dotenv.config()
@@ -13,20 +11,6 @@ const PORT = process.env.PORT
 const app = express();
 app.use(cors())
 app.use("/order", orderRouter)
-
-app.post("/create_order", async (req: Request, res: Response) => {
-    const params = req.params
-    const nextOrderNr = await getNextNumber(CounterEnum.Order)
-    const orderNr = CounterEnum.Order + nextOrderNr
-
-    if (nextOrderNr !== 0) {
-        const order = await Order.create({ _id: orderNr })
-        res.json(order)
-        return 0
-    }
-
-    res.status(500).json({ "error": 'the order cannot be created' })
-})
 
 connectToMongo()
 
--- src/controllers/order.ts
+++ src/controllers/order.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { Error } from "mongoose";
+import { CounterEnum, getNextNumber } from "../models/Counter";
 import Order from "../models/Order";
 const orderRouter = Router();
 
@@ -11,6 +12,20 @@ orderRouter.get("/orders", async (req: Request, res: Response) => {
     }).catch((err: Error) => {
         res.json(err)
     })
+})
+
+orderRouter.post("/create_order", async (req: Request, res: Response) => {
+    const params = req.params
+    const nextOrderNr = await getNextNumber(CounterEnum.Order)
+    const orderNr = CounterEnum.Order + nextOrderNr
+
+    if (nextOrderNr !== 0) {
+        const order = await Order.create({ _id: orderNr })
+        res.json(order)
+        return 0
+    }
+
+    res.status(500).json({ "error": 'the order cannot be created' })
 })
 
 export { orderRouter }
